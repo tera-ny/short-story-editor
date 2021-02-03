81547c13d9218c9dd7a68bb47b5ba6cc86417859
g4zeru <haru25haru02@gmail.com>
2021-02-03 22:51:24 +0900

    replace styled-jsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
-import {AppProps} from 'next/app'
+import { AppProps } from "next/app";
+import _JSXStyle from "styled-jsx/style";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
 
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </>
+  );
+}
